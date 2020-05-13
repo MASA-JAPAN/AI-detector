19aac8befa22cfeebea3d9b5d39a04f692135646
MASA-JAPAN <masaifactoryline@gmail.com>
2020-05-13 23:07:09 +0900

    [Fix] change top page with a video

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,48 @@ import Router from "next/router";
 
 const IndexPage = () => (
   <div>
-    <div className="title">WIT</div>
-    <div className="description">What is this?</div>
-    <div
-      className="startButton"
-      onClick={() =>
-        Router.push({
-          pathname: "/detect",
-        })
-      }
-    >
-      <div className="startRectangle">
-        <div className="textStart">Start</div>
+    <div className="container">
+      <video muted autoPlay loop className="topVideo">
+        <source src="/videos/top.mp4" type="video/mp4" />
+      </video>
+      <div className="title">WIT</div>
+      <div className="description">What is this?</div>
+      <div
+        className="startButton"
+        onClick={() =>
+          Router.push({
+            pathname: "/detect",
+          })
+        }
+      >
+        <div className="startRectangle">
+          <div className="textStart">Start</div>
+        </div>
       </div>
-    </div>
-    <div className="galleryButton">
-      <div className="galleryRectangle">
-        <div className="textGallery">Gallery</div>
+      <div className="galleryButton">
+        <div className="galleryRectangle">
+          <div className="textGallery">Gallery</div>
+        </div>
       </div>
     </div>
 
     <style jsx>
       {`
+        .container {
+          position: relative;
+          overflow: hidden;
+          width: 100vw;
+          height: 100vh;
+        }
+        .topVideo {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          min-width: 100vw;
+          min-height: 100vh;
+        }
+
         .title {
           position: absolute;
           width: 278px;
@@ -42,6 +62,33 @@ const IndexPage = () => (
           letter-spacing: 0.215em;
 
           color: #000000;
+          text-shadow: rgb(232, 60, 60) 1px 0 10px;
+          animation: beatShadow 1.5s ease-in-out infinite both;
+        }
+
+        @keyframes beatShadow {
+          0%,
+          100% {
+            text-shadow: rgb(232, 60, 60) 1px 0 10px;
+            transform-origin: center center;
+            animation-timing-function: ease-out;
+          }
+          10% {
+            text-shadow: rgb(232, 60, 60) 1px 0 15px;
+            animation-timing-function: ease-in;
+          }
+          17% {
+            text-shadow: rgb(232, 60, 60) 1px 0 28px;
+            animation-timing-function: ease-out;
+          }
+          33% {
+            text-shadow: rgb(232, 60, 60) 1px 0 13px;
+            animation-timing-function: ease-in;
+          }
+          45% {
+            text-shadow: rgb(232, 60, 60) 1px 0 11px;
+            animation-timing-function: ease-out;
+          }
         }
 
         .description {
